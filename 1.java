b6d4259b0471571fb70ea2fe3c125f5ea51111f0
qiqiyiyiguo <3370256643@qq.com>
2024-10-13 21:19:20 +0800

    add 1.java

---
+++ 1.java
@@ -0,0 +1,11 @@
+Integer x = new Integer(18);
+Integer y = new Integer(18);
+System.out.println(x == y);
+
+Integer z = Integer.valueOf(18);
+Integer k = Integer.valueOf(18);
+System.out.println(z == k);
+
+Integer m = Integer.valueOf(300);
+Integer p = Integer.valueOf(300);
+System.out.println(m == p);
